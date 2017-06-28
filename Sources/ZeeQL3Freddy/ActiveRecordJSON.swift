aa3dd353d2a39c825dc75e55f01daab8cb7b31a6
Helge Hess <devteam@zeezide.de>
2017-06-28 18:06:58 +0200

    Prep JSONEncodableType
    ... not done yet.

--- Sources/ZeeQL3Freddy/ActiveRecordJSON.swift
+++ Sources/ZeeQL3Freddy/ActiveRecordJSON.swift
@@ -12,6 +12,9 @@ import Freddy
 extension ZeeQL.ActiveRecord : Freddy.JSONEncodable {
 }
 
+// TBD: We cannot make it JSONDecodable because we first need the entity,
+//      right?
+
 public extension ActiveRecordType {
   
   public func toJSON() -> JSON {
@@ -48,3 +51,62 @@ public extension Entity {
   }
   
 }
+
+public enum JSONAttributeType {
+  case array, dictionary, double, int, string, bool, null
+}
+
+public protocol JSONEncodableType {
+  
+  public var jsonAttributeType : JSONAttributeType { get }
+  
+}
+
+extension String : JSONEncodableType {
+  public var jsonAttributeType : JSONAttributeType { return .string }
+}
+
+#if false // TODO:
+extension Data    : JSONEncodableType
+extension Date    : JSONEncodableType
+extension Decimal : JSONEncodableType
+  #endif
+
+extension Int     : JSONEncodableType {
+  public var jsonAttributeType : JSONAttributeType { return .int }
+}
+extension Int16   : JSONEncodableType {
+  public var jsonAttributeType : JSONAttributeType { return .int }
+}
+extension Int32   : JSONEncodableType {
+  public var jsonAttributeType : JSONAttributeType { return .int }
+}
+extension Int64   : JSONEncodableType {
+  public var jsonAttributeType : JSONAttributeType { return .int }
+}
+extension Float   : JSONEncodableType {
+  public var jsonAttributeType : JSONAttributeType { return .double }
+}
+extension Double  : JSONEncodableType {
+  public var jsonAttributeType : JSONAttributeType { return .double }
+}
+extension Bool    : JSONEncodableType {
+  public var jsonAttributeType : JSONAttributeType { return .bool }
+}
+
+extension URL     : JSONEncodableType {
+  public var jsonAttributeType : JSONAttributeType { return .string }
+}
+
+public extension Attribute {
+  
+  public var jsonAttributeType : JSONAttributeType {
+    // TODO: support a userInfo key to override the mapping
+    
+    if let v = self.valueType {
+    }
+    
+    return .string // TBD: or make it optional?
+  }
+}
+

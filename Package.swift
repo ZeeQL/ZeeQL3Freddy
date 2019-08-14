e8f5d5ae1c8b06224573b3fc0adfec695e507b98
Helge Heß <devteam@zeezide.de>
2019-08-14 13:16:11 +0200

    Swift 4.2/5 upgrade
    Prep project for 4.2.

--- Package.swift
+++ Package.swift
@@ -1,23 +1,20 @@
+// swift-tools-version:4.2
+
 import PackageDescription
 
 let package = Package(
   name: "ZeeQL3Freddy",
 
-  targets: [
-    Target(name: "ZeeQL3Freddy")
+  products: [
+    .library(name: "ZeeQL3Freddy", targets: [ "ZeeQL3Freddy" ])
   ],
-  
   dependencies: [
-    .Package(url: "https://github.com/ZeeQL/ZeeQL3.git",  majorVersion: 0),
-    .Package(url: "https://github.com/modswift/Freddy.git",
-             majorVersion: 3, minor: 0)
+    .package(url: "https://github.com/ZeeQL/ZeeQL3.git",
+             from: "0.8.0"),
+    .package(url: "https://github.com/modswift/Freddy.git",
+             from: "3.0.58")
   ],
-  
-  exclude: [
-    "ZeeQL3Freddy.xcodeproj",
-    "GNUmakefile",
-    "LICENSE",
-    "README.md",
-    "xcconfig"
+  targets: [
+    .target(name: "ZeeQL3Freddy", dependencies: [ "ZeeQL", "Freddy" ])
   ]
 )
